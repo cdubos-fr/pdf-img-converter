734f36b3d711af8e8a72984a55254ac879a1a427
cdubos <contact@cdubos.fr>
2023-03-30 20:12:38 +0200

    feat: setup cli

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple cli to convert pdf to images and back.
--- pdf_converter.py
+++ pdf_converter.py
@@ -1,7 +1,15 @@
+"""A simple cli to convert pdf to images and back."""
+__version__ = "0.1.0"
 import os
 from pdf2image import convert_from_path
 from PIL import Image
 import contextlib
+from typer import Typer
+from rich.console import Console
+
+console = Console()
+cli = Typer()
+
 
 def resolv_path(path: str) -> tuple[str, str]:
     dest = os.path.dirname(path)
@@ -10,7 +18,7 @@ def resolv_path(path: str) -> tuple[str, str]:
     return dest, filename
 
 
-
+@cli.command("pdf-to-img")
 def convert_pdf_to_img(path: str) -> list[str]:
     dest, filename = resolv_path(path)
     pages = convert_from_path(path)
@@ -19,9 +27,10 @@ def convert_pdf_to_img(path: str) -> list[str]:
         file = os.path.join(dest, f'{filename}_{idx}.jpg')
         page.save(file, 'JPEG')
         files.append(file)
+    console.print(path, "converted to", ", ".join(files))
     return files
 
-
+@cli.command("imgs-to-pdf")
 def convert_imgs_to_pdf(images_path: list[str]) -> str:
     dest, filename = resolv_path(images_path[0])
     filebase, *_ = filename.rpartition('_')
@@ -34,4 +43,5 @@ def convert_imgs_to_pdf(images_path: list[str]) -> str:
             img_list.append(img)
         file = os.path.join(dest, f'{filebase}.pdf')
         img_list[0].save(file, save_all=True, append_images=img_list[1:])
+    console.print(", ".join(images_path), "merged in", file)
     return file
