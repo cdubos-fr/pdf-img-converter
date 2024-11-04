3ad24d1a826d74476ac835c7b9a3b60ada4af533
cdubos-fr <contact@cdubos.fr>
2024-11-04 18:00:51 +0000

    fix: incorrect filename name inference if not underscore

--- pdf_converter.py
+++ pdf_converter.py
@@ -19,22 +19,23 @@ def resolv_path(path: str) -> tuple[str, str]:
 
 
 @cli.command("pdf-to-img")
-def convert_pdf_to_img(path: str) -> list[str]:
+def convert_pdf_to_img(path: str, output_folder: str | None = None) -> list[str]:
     dest, filename = resolv_path(path)
+    if not output_folder:
+        output_folder = dest
     pages = convert_from_path(path)
     files = []
     for idx, page in enumerate(pages):
-        file = os.path.join(dest, f'{filename}_{idx}.jpg')
-        page.save(file, 'JPEG')
-        files.append(file)
+        output_file = os.path.join(output_folder, f'{filename}_{idx}.jpg')
+        page.save(output_file, 'JPEG')
+        files.append(output_file)
     console.print(path, "converted to", ", ".join(files))
     return files
 
 
 @cli.command("imgs-to-pdf")
-def convert_imgs_to_pdf(images_path: list[str]) -> str:
-    dest, filename = resolv_path(images_path[0])
-    filebase, *_ = filename.rpartition('_')
+def convert_imgs_to_pdf(images_path: list[str], output_folder: str | None = None) -> str:
+
     with contextlib.ExitStack() as stack:
         img_list = []
         for image_path in images_path:
@@ -42,7 +43,11 @@ def convert_imgs_to_pdf(images_path: list[str]) -> str:
             stack.enter_context(img)
             img.convert('RGB')
             img_list.append(img)
-        file = os.path.join(dest, f'{filebase}.pdf')
-        img_list[0].save(file, save_all=True, append_images=img_list[1:])
-    console.print(", ".join(images_path), "merged in", file)
-    return file
+        if not output_folder:
+            dest, filename = resolv_path(images_path[0])
+            output_folder = dest
+        filebase, *_ = filename.rsplit('_')
+        output_file = os.path.join(output_folder, f'{filebase}.pdf')
+        img_list[0].save(output_file, save_all=True, append_images=img_list[1:])
+    console.print(", ".join(images_path), "merged in", output_file)
+    return output_file
